1b02ca307c2d68128db844f97e0cd0188e7bf991
lx32056127 <30209425+lx32056127@users.noreply.github.com>
2021-04-29 17:07:48 +0800

    fix auto categor time bug.

--- app/src/main/java/cn/dreamn/qianji_auto/ui/fragment/category/JsFragment.java
+++ app/src/main/java/cn/dreamn/qianji_auto/ui/fragment/category/JsFragment.java
@@ -105,7 +105,8 @@ public class JsFragment extends BaseFragment {
             final MaterialEditText cate_shopRemark = textEntryView.findViewById(R.id.cate_shopRemark);
             final RoundButton cate_type = textEntryView.findViewById(R.id.cate_type);
             final RoundButton cate_type2 = textEntryView.findViewById(R.id.cate_type2);
-            cate_time.setText(Tools.getTime("HH"));
+            cate_time.setText(Caches.getOneString("cate_time", Tools.getTime("yyyy-MM-dd HH:mm:ss")));
+            cate_money.setText(Caches.getOneString("cate_money", "0"));
             cate_type.setText("支出");
             cate_type.setOnClickListener(v22 -> {
                 new MaterialDialog.Builder(getContext())
@@ -137,6 +138,8 @@ public class JsFragment extends BaseFragment {
                     .positiveText("测试")
                     .onPositive((dialog, which) -> {
                         String js = js_data.getContentText();
+                        Caches.AddOrUpdate("cate_time", cate_time.getEditValue());
+                        Caches.AddOrUpdate("cate_money", cate_money.getEditValue());
                         Caches.AddOrUpdate("cate_shopName", cate_shopName.getEditValue());
                         Caches.AddOrUpdate("cate_shopRemark", cate_shopRemark.getEditValue());
                         try {
