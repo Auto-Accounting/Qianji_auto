afca6b75db753faccb9f8560c6a747f167f5e4a9
ankio <dream@dreamn.cn>
2021-05-10 10:54:08 +0800

    Delete something
    Signed-off-by: Your Name <you@example.com>

--- app/src/main/java/cn/dreamn/qianji_auto/ui/adapter/BookSelectListAdapter.java
+++ app/src/main/java/cn/dreamn/qianji_auto/ui/adapter/BookSelectListAdapter.java
@@ -1,22 +1,5 @@
 package cn.dreamn.qianji_auto.ui.adapter;
 
-/*
- * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -29,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -39,7 +21,7 @@ import cn.dreamn.qianji_auto.utils.pictures.MyBitmapUtils;
 
 public class BookSelectListAdapter extends ArrayAdapter {
 
-    private Context mContext;
+    private final Context mContext;
     public BookSelectListAdapter(Context context, Bundle[] bundles) {
         super(context, R.layout.book_item2, bundles);
         mContext=context;
--- app/src/main/java/cn/dreamn/qianji_auto/ui/fragment/data/LogFragment.java
+++ app/src/main/java/cn/dreamn/qianji_auto/ui/fragment/data/LogFragment.java
@@ -231,5 +231,4 @@ public class LogFragment extends BaseFragment {
         }));
     }
 
-
 }
