9aa8dbc4c8b969101c40636aecda67e4af218852
ankio <dream@dreamn.cn>
2021-06-19 17:59:32 +0800

    Modified the value logic of the JS section
    Signed-off-by: ankio <dream@dreamn.cn>

--- app/src/main/assets/html/Public/jquery/DataUtils.js
+++ app/src/main/assets/html/Public/jquery/DataUtils.js
@@ -13,7 +13,24 @@ let Base64 = {
         return decode(str,"base16");
     }
 };
-
+let Html = {
+     stringToEntity(str,radix){
+        let arr=str.split('')
+        radix=radix||0
+        let tmp=arr.map(item=>
+            `&#${(radix?'x'+item.charCodeAt(0).toString(16):item.charCodeAt(0))};`).join('')
+        console.log(`'${str}' 转实体为 '${tmp}'`)
+        return tmp
+    },
+     entityToString(entity){
+        let entities=entity.split(';')
+        entities.pop()
+        let tmp=entities.map(item=>String.fromCharCode(
+            item[2]==='x'?parseInt(item.slice(3),16):parseInt(item.slice(2)))).join('')
+        console.log(`'${entity}' 转字符串为 '${tmp}'`)
+        return tmp
+    }
+}
 //let encoded = Base64.encode("哈ha"); // "5ZOIaGE="
 //let decoded = Base64.decode(encoded); // "哈ha"
 class DataUtils{
